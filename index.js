a63e33216f41d1b4d86ae0ae829b9979fa925463
gooseisdead <gooseisdead@mac.com>
2020-12-21 20:09:38 -0500

    start

---
+++ index.js
@@ -0,0 +1,17 @@
+//Declare variables in camelCase
+let coolVariable
+//Assignments
+coolVariable = "go dodgers"
+//Combine
+const coolerVariable = "GO DODGERS!"
+
+console.log(coolVariable)
+console.log(coolerVariable)
+
+// + , - , * , / ,
+// Increment/Decremnet numbers with ++ , --
+
+
+
+
+
